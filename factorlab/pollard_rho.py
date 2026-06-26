060b66dff9c2aa9106dd41881395ce75d281b255
Steven <qiwen_zhu@qq.com>
2026-06-26 20:38:40 +0100

    Pollard-Rho implemented

--- factorlab/pollard_rho.py
+++ factorlab/pollard_rho.py
@@ -51,4 +51,77 @@ def _f(x: mpz, c: mpz, n: mpz) -> mpz:
     return (x * x + c) % n
 
 def pollard_rho(n: mpz) -> mpz:
-    raise NotImplementedError
+    """
+    Find a non-trivial factor of an odd composite integer using
+    Pollard's Rho algorithm.
+
+    Pollard's Rho performs a pseudo-random walk over the integers
+    modulo n using the polynomial
+
+        f(x) = x² + c (mod n)
+
+    Two walkers are advanced using Floyd's cycle-finding algorithm.
+    Whenever
+
+        gcd(|x - y|, n)
+
+    becomes greater than one, a non-trivial factor has been found.
+
+    Since the algorithm is probabilistic, a single run may fail.
+    In that case the random walk is restarted with different initial
+    parameters until a factor is discovered.
+
+    Parameters
+    ----------
+    n : mpz
+        Odd composite integer to be factored.
+
+    Returns
+    -------
+    mpz
+        A non-trivial factor of n.
+
+    Raises
+    ------
+    ValueError
+        If n < 2.
+
+    Notes
+    -----
+    This implementation uses Floyd's tortoise-and-hare cycle
+    detection algorithm.
+
+    The expected running time is approximately
+
+        O(n^(1/4))
+
+    for integers having two similarly-sized prime factors.
+    """
+
+    if n < 2:
+        raise ValueError("n must be at least 2")
+
+    if n % 2 == 0:
+        return mpz(2)
+
+    if is_prime(n):
+        return n
+
+    while True:
+
+        # Random starting point
+        x = ...
+        y = ...
+        c = ...
+
+        d = mpz(1)
+
+        while d == 1:
+
+            x = _f(x, c, n)
+            y = _f(_f(y, c, n), c, n)
+
+            d = gcd(abs(x - y), n)
+
+        if d != n:
+            return d
